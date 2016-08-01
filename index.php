77ad05d8cd732db72d6e3536acd9e78f89ec4f5c
Luca Vercellio <luca@reset-lab.it>
2016-08-01 16:01:07 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+	<main>
+		<!-- section -->
+		<section>
+		
+			<?php get_template_part('loop'); ?>
+
+			<?php get_template_part('pagination'); ?>
+
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<a class="sidebar-button"></a>
+<!-- sidebar -->
+<aside class="sidebar">
+	<h2>Sidebar</h2>
+
+	<div class="sidebar-widget">
+		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-1')) ?>
+	</div>
+
+</aside>
+<!-- /sidebar -->
